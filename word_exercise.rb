0603ae30cb68779b1263bd7baa2da211337e58ec
Joe Donahue <joe.donahue@simplybusiness.com>
2023-07-27 15:27:51 -0500

    Added logic for adjectives.

--- word_exercise.rb
+++ word_exercise.rb
@@ -1,15 +1,35 @@
 require_relative 'word_type_comparer'
 
 class WordExercise
-  def initialize(word)
+  def initialize(sentence)
+    @sentence = sentence
+    word = @sentence.split(' ').last
+
     @noun_comparer = WordTypeComparer.new(word, 'noun')
+    @adjective_comparer = WordTypeComparer.new(word, 'adjective')
+  end
+
+  def generate_next_word
+
+  end
+
+  def display_sentence
+
   end
 
   def is_a_noun?
-    puts @noun_comparer.is_type?
+    @noun_comparer.is_type?
+  end
+
+  def is_a_adjective?
+    @adjective_comparer.is_type?
   end
 
   def pick_random_noun
-    puts @noun_comparer.pick_random_word_from_type
+    @noun_comparer.pick_random_word_from_type
+  end
+
+  def pick_random_adjective
+    @adjective_comparer.pick_random_word_from_type
   end
 end
